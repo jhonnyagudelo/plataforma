78ef2c7a414d956afe8af34c8eb05d4244cfa51f
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2020-04-09 10:42:38 -0500

    mostarUsuario responsive

--- js/programacion.js
+++ js/programacion.js
@@ -1,9 +1,9 @@
-const $detailsList = document.querySelectorAll('details')
-$detailsList.forEach(($details) => {
-  $details.querySelector('summary').addEventListener('click', expand)
-})
-function expand() {
-  $detailsList.forEach(($details) => {
-    $details.removeAttribute('open')
-  })
-}
+// const $detailsList = document.querySelectorAll('details')
+// $detailsList.forEach(($details) => {
+//   $details.querySelector('summary').addEventListener('click', expand)
+// })
+// function expand() {
+//   $detailsList.forEach(($details) => {
+//     $details.removeAttribute('open')
+//   })
+// }
